6adbc413fbe2d9fb3397b09a4ccfd7153c01ce4b
ldcnft <101517909+ldcnft@users.noreply.github.com>
2022-07-29 00:25:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -158,8 +158,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 66) {
-      newMintAmount = 66;
+    if (newMintAmount > 1) {
+      newMintAmount = 1;
     }
     setMintAmount(newMintAmount);
   };
@@ -379,49 +379,49 @@ function App() {
                     <s.SpacerMedium />
                         
 <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          tenMintAmount();
-                          getData();
-                        }}
-                      >
-                        10
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          twentyMintAmount();
-                          getData();
-                        }}
-                      >
-                        20
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          thirtyMintAmount();
-                          getData();
-                        }}
-                      >
-                        30
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          maxMintAmount();
-                          getData();
-                        }}
-                      >
-                        Max
-                      </StyledRoundButton>
+//                       <StyledRoundButton
+//                         disabled={claimingNft ? 1 : 0}
+//                         onClick={(e) => {
+//                           e.preventDefault();
+//                           tenMintAmount();
+//                           getData();
+//                         }}
+//                       >
+//                         10
+//                       </StyledRoundButton>
+// <s.SpacerMedium />
+//                       <StyledRoundButton
+//                         disabled={claimingNft ? 1 : 0}
+//                         onClick={(e) => {
+//                           e.preventDefault();
+//                           twentyMintAmount();
+//                           getData();
+//                         }}
+//                       >
+//                         20
+//                       </StyledRoundButton>
+// <s.SpacerMedium />
+//                       <StyledRoundButton
+//                         disabled={claimingNft ? 1 : 0}
+//                         onClick={(e) => {
+//                           e.preventDefault();
+//                           thirtyMintAmount();
+//                           getData();
+//                         }}
+//                       >
+//                         30
+//                       </StyledRoundButton>
+// <s.SpacerMedium />
+//                       <StyledRoundButton
+//                         disabled={claimingNft ? 1 : 0}
+//                         onClick={(e) => {
+//                           e.preventDefault();
+//                           maxMintAmount();
+//                           getData();
+//                         }}
+//                       >
+//                         Max
+//                       </StyledRoundButton>
                     </s.Container>
                     <s.SpacerSmall />
                     <s.Container ai={"center"} jc={"center"} fd={"row"}>
