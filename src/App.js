1e3095591abe1db38bf11e041cde3d3d86da02e8
ldcnft <101517909+ldcnft@users.noreply.github.com>
2022-11-03 23:26:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
+import { InputNumber } from 'primereact/inputnumber';
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
 
 export const StyledButton = styled.button`
   padding: 10px;
-  border-radius: 50px;
+  font-size: 18px;
   border: none;
-  background-color: var(--secondary);
+  background-color: transparent;
   padding: 10px;
   font-weight: bold;
   color: var(--secondary-text);
@@ -27,9 +28,9 @@ export const StyledButton = styled.button`
 
 export const StyledRoundButton = styled.button`
   padding: 10px;
-  border-radius: 100%;
   border: none;
   background-color: var(--primary);
+  background-color: transparent;
   padding: 10px;
   font-weight: bold;
   font-size: 15px;
@@ -89,240 +90,116 @@ export const StyledLink = styled.a`
   text-decoration: none;
 `;
 
-function App() {
-  const dispatch = useDispatch();
-  const blockchain = useSelector((state) => state.blockchain);
-  const data = useSelector((state) => state.data);
-  const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
-  const [approvefeedback, setAproveFeedback] = useState(`If You Want To Stake Your NFT Click Approve`);
-  const [mintAmount, setMintAmount] = useState(1);
-  const [CONFIG, SET_CONFIG] = useState({
-    CONTRACT_ADDRESS: "",
-    SCAN_LINK: "",
-    NETWORK: {
-      NAME: "",
-      SYMBOL: "",
-      ID: 0,
-    },
-    NFT_NAME: "",
-    SYMBOL: "",
-    MAX_SUPPLY: 1,
-    WEI_COST: 0,
-    DISPLAY_COST: 0,
-    GAS_LIMIT: 0,
-    MARKETPLACE: "",
-    MARKETPLACE_LINK: "",
-    SHOW_BACKGROUND: false,
-  });
-  
-  const approveNFTs = () => {
-    setAproveFeedback(`Approving your ${CONFIG.NFT_NAME}, Check your wallet is connected first ...`);
-    blockchain.smartContract.methods
-    .setApprovalForAll("0x4D8C6bFEA02A8739D587F7BB9B76D5BB581eB9c6",true)
-    .send({
-      to: CONFIG.CONTRACT_ADDRESS,
-      from: blockchain.account,
-    })
-    .once("error", (err) => {
-      console.log(err);
-      setAproveFeedback("Sorry, something went wrong please try again later.");
-    })
-    .then((receipt) => {
-      console.log(receipt);
-      setAproveFeedback(
-        `WOW, the ${CONFIG.NFT_NAME} is ready for staking! go stake.fancydragons.xyz to stake it.`
-      );
-      dispatch(fetchData(blockchain.account));
-    });
 
-  }
-
-  const claimNFTs = () => {
-    let cost = CONFIG.WEI_COST;
-    let gasLimit = CONFIG.GAS_LIMIT;
-    let totalCostWei = String(cost * mintAmount);
-    let totalGasLimit = String(gasLimit * mintAmount);
-    console.log("Cost: ", totalCostWei);
-    console.log("Gas limit: ", totalGasLimit);
-    setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
-    setClaimingNft(true);
-    blockchain.smartContract.methods
-      .mint(blockchain.account, mintAmount)
-      .send({
-        gasLimit: String(totalGasLimit),
-        to: CONFIG.CONTRACT_ADDRESS,
-        from: blockchain.account,
-        value: totalCostWei,
-      })
-      .once("error", (err) => {
-        console.log(err);
-        setFeedback("Sorry, something went wrong please try again later.");
-        setClaimingNft(false);
-      })
-      .then((receipt) => {
-        console.log(receipt);
-        setFeedback(
-          `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
-        );
-        setClaimingNft(false);
-        dispatch(fetchData(blockchain.account));
-      });
-  };
-
-  const decrementMintAmount = () => {
-    let newMintAmount = mintAmount - 1;
-    if (newMintAmount < 1) {
-      newMintAmount = 1;
-    }
-    setMintAmount(newMintAmount);
-  };
-
-  const incrementMintAmount = () => {
-    let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 6) {
-      newMintAmount = 6;
-    }
-    setMintAmount(newMintAmount);
-  };
-
-    const tenMintAmount = () => {
-    let newMintAmount = 10;
-
-    setMintAmount(newMintAmount);
-  };
-    const twentyMintAmount = () => {
-    let newMintAmount = 20;
-
-    setMintAmount(newMintAmount);
-  };
-  
-    const thirtyMintAmount = () => {
-    let newMintAmount = 30;
-    setMintAmount(newMintAmount);
-  };
-  
-      const maxMintAmount = () => {
-    let newMintAmount = 66;
-
-    setMintAmount(newMintAmount);
-  };
-
-  const getData = () => {
-    if (blockchain.account !== "" && blockchain.smartContract !== null) {
-      dispatch(fetchData(blockchain.account));
-    }
-  };
-
-  const getConfig = async () => {
-    const configResponse = await fetch("/config/config.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
+  function App() {
+    const dispatch = useDispatch();
+    const blockchain = useSelector((state) => state.blockchain);
+    const data = useSelector((state) => state.data);
+    const [value, setValue] = useState(1);
+    const [feedback, setFeedback] = useState(`If You Want To Stake Your NFT Click Approve`);
+    const [CONFIG, SET_CONFIG] = useState({
+      CONTRACT_ADDRESS: "",
+      SCAN_LINK: "",
+      NETWORK: {
+        NAME: "",
+        SYMBOL: "",
+        ID: 0,
       },
+      NFT_NAME: "",
+      SYMBOL: "",
+      MAX_SUPPLY: 1,
+      WEI_COST: 0,
+      DISPLAY_COST: 0,
+      GAS_LIMIT: 0,
+      MARKETPLACE: "",
+      MARKETPLACE_LINK: "",
+      SHOW_BACKGROUND: false,
     });
-    const config = await configResponse.json();
-    SET_CONFIG(config);
-  };
 
-  useEffect(() => {
-    getConfig();
-  }, []);
 
-  useEffect(() => {
-    getData();
-  }, [blockchain.account]);
 
-  return (
-    <s.Screen>
-      <s.Container
-        flex={1}
-        ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
-      >
-        <a href={CONFIG.MARKETPLACE_LINK}>
-          <StyledLogo alt={"logo"} src={"/config/logo.png"} />
-        </a>
-        <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
-          <s.SpacerLarge />
-          <s.Container
-            flex={2}
-            jc={"center"}
-            ai={"center"}
-            style={{
-            }}
-          >
-            <s.TextTitle
-              style={{
-                textAlign: "center",
-                fontSize: 50,
-                fontWeight: "bold",
-                color: "var(--accent-text)",
-              }}
-            >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
-            </s.TextTitle>
+
+    const approveNFTs = () => {
+      setFeedback(`Approving your ${CONFIG.NFT_NAME}...`);
+      blockchain.smartContract.methods
+        .stake(String(value))
+        .send({
+          to: CONFIG.CONTRACT_ADDRESS,
+          from: blockchain.account,
+        })
+        .once("error", (err) => {
+          console.log(err);
+          setFeedback("Sorry, something went wrong please try again later.");
+        })
+        .then((receipt) => {
+          console.log(receipt);
+          setFeedback(
+            `WOW, the ${CONFIG.NFT_NAME} is ready for staking! go <a href='https://google.com'>HERE</a> to stake it.`
+          );
+          dispatch(fetchData(blockchain.account));
+        });
+
+    }
+
+
+
+
+
+    const getData = () => {
+      if (blockchain.account !== "" && blockchain.smartContract !== null) {
+        dispatch(fetchData(blockchain.account));
+      }
+    };
+
+    const getConfig = async () => {
+      const configResponse = await fetch("/config/config.json", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const config = await configResponse.json();
+      SET_CONFIG(config);
+    };
+
+    useEffect(() => {
+      getConfig();
+    }, []);
+
+    useEffect(() => {
+      getData();
+    }, [blockchain.account]);
+
+    return (
+      <s.Screen>
+        <s.Container
+          flex={1}
+          ai={"center"}
+          style={{ padding: 24 }}
+        >
+          <s.SpacerSmall />
+          <div>
+          <div className="card">
+              <h5>Token Number</h5>
+              <div className="p-fluid grid formgrid">
+                  <div className="field col-12 md:col-3">
+                      <InputNumber inputId="minmax-buttons" value={value} onValueChange={(e) => setValue(e.value)} mode="decimal" min={1} max={6666} />
+                  </div>
+              </div>
+
+          </div>
+      </div>
+          <s.Container jc={"center"} ai={"center"} style={{ width: "70%", marginBottom: "100px" }}>
             <s.TextDescription
               style={{
                 textAlign: "center",
-                color: "var(--primary-text)",
+                color: "var(--accent-text)",
               }}
             >
-              <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
-                {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
-              </StyledLink>
-             
+              If You Want To Stake Your NFT Click Approve
             </s.TextDescription>
-            <span
-              style={{
-                textAlign: "center",
-              }}
-            >
-              <StyledButton
-                style={{
-                  margin: "5px",
-                }}
-                onClick={(e) => {
-                  window.open(CONFIG.MARKETPLACE_LINK, "_blank");
-                }}
-              >
-                {CONFIG.MARKETPLACE}
-              </StyledButton>
-            </span>
-            <s.SpacerSmall />
-            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
-              <>
-                <s.TextTitle
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
-                  The sale has ended.
-                </s.TextTitle>
-                <s.TextDescription
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
-                  You can still find {CONFIG.NFT_NAME} on
-                </s.TextDescription>
-                <s.SpacerSmall />
-                <StyledLink target={"_blank"} href={CONFIG.MARKETPLACE_LINK}>
-                  {CONFIG.MARKETPLACE}
-                </StyledLink>
-              </>
-            ) : (
-              <>
-                <s.TextTitle
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
-                  1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
-                  {CONFIG.NETWORK.SYMBOL}.
-                </s.TextTitle>
-               <s.TextTitle
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
-                </s.TextTitle>
-                <s.SpacerXSmall />
-                <s.SpacerSmall />
-                {blockchain.account === "" ||
+
+            {blockchain.account === "" ||
                 blockchain.smartContract === null ? (
                   <s.Container ai={"center"} jc={"center"}>
                     <s.TextDescription
@@ -357,165 +234,32 @@ function App() {
                       </>
                     ) : null}
                   </s.Container>
-                ) : (
-                  <>
-                    <s.TextDescription
-                      style={{
-                        textAlign: "center",
-                        color: "var(--accent-text)",
-                      }}
-                    >
-                      {feedback}
-                    </s.TextDescription>
-                    <s.SpacerMedium />
-                    <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                      <StyledRoundButton
-                        style={{ lineHeight: 0.4 }}
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          decrementMintAmount();
-                        }}
-                      >
-                        -
-                      </StyledRoundButton>
-                      <s.SpacerMedium />
-                      <s.TextDescription
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
-                        {mintAmount}
-                      </s.TextDescription>
-                      <s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          incrementMintAmount();
-                        }}
-                      >
-                        +
-                      </StyledRoundButton>
-                    </s.Container>
-
-<s.SpacerMedium />
-                        
-                        <s.Container ai={"center"} jc={"center"} fd={"row"}>
-
-  
-  
-  
-                        <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          tenMintAmount();
-                          getData();
-                        }}
-                      >
-                        10
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          twentyMintAmount();
-                          getData();
-                        }}
-                      >
-                        20
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          thirtyMintAmount();
-                          getData();
-                        }}
-                      >
-                        30
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          maxMintAmount();
-                          getData();
-                        }}
-                      >
-                        Max
-                      </StyledRoundButton>
-  
-  
-  
-  
-  
-                    </s.Container>
-                        
-
-                    <s.SpacerSmall />
-                    <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                      <StyledButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          claimNFTs();
-                          getData();
-                        }}
-                      >
-                        {claimingNft ? "BUSY" : "BUY"}
-                      </StyledButton>
-                    </s.Container>
-                  </>
-                )}
+                                  ) : (
+                                    <>
+                                                    
               </>
-            )}
-            <s.SpacerMedium />
+                              )}
+
+
+            <s.SpacerSmall />
+            <StyledButton
+              onClick={(e) => {
+                e.preventDefault();
+                approveNFTs();
+                getData();
+              }}
+            >
+              Stake
+            </StyledButton>
           </s.Container>
-          <s.SpacerLarge />
-        </ResponsiveWrapper>
-        <s.Container jc={"center"} ai={"center"} style={{ width: "70%", marginBottom: "100px" }}>
-        <s.TextDescription
-                      style={{
-                        textAlign: "center",
-                        color: "var(--accent-text)",
-                      }}
-                    >
-                      {approvefeedback}
-                    </s.TextDescription>
-          <s.SpacerSmall />
-          <StyledButton
-                        onClick={(e) => {
-                          e.preventDefault();
-                          approveNFTs();
-                          getData();
-                        }}
-                      >
-                       APPROVE
-                      </StyledButton>
-        </s.Container>
-        <s.SpacerMedium />
-        <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
-          <s.TextDescription
-            style={{
-              textAlign: "center",
-              color: "var(--primary-text)",
-            }}
-          >
-            Please make sure you are connected to the right network (
-            {CONFIG.NETWORK.NAME} Mainnet) and the correct address.
-          </s.TextDescription>
-          <s.SpacerSmall />
-        </s.Container>
-      </s.Container>
-    </s.Screen>
-  );
-}
+          <s.SpacerMedium />
+          <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
 
+            <s.SpacerSmall />
+          </s.Container>
+        </s.Container>
+      </s.Screen>
+    );
+  }
 
-export default App;
+  export default App;
