cb48b873cf712344fb9d58de9febd3581f3b9781
ldcnft <101517909+ldcnft@users.noreply.github.com>
2022-11-07 16:44:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
 import { InputNumber } from 'primereact/inputnumber';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css'
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
